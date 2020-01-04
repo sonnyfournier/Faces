94b90982183b2496db346ab7c8042477a3052435
Sonny Fournier <sfournier@big-boss-studio.com>
2020-01-04 03:58:11 +0100

    :construction: Try UIPanGestureRecognizer for joystick, missing limits

--- Faces/Faces/ViewController.swift
+++ Faces/Faces/ViewController.swift
@@ -14,39 +14,27 @@ class ViewController: UIViewController {
     let joystickSize = 100
     let substractSize = 200
 
-    let scrollView = UIScrollView()
     let joystickSubstractView = UIView()
     let joystickView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        scrollView.delegate = self
-        scrollView.backgroundColor = .blue
-        scrollView.bounces = false
-        scrollView.clipsToBounds = true
-        scrollView.showsVerticalScrollIndicator = false
-        scrollView.showsHorizontalScrollIndicator = false
-        scrollView.layer.cornerRadius = CGFloat(substractSize / 2)
-        view.addSubview(scrollView)
-
         joystickSubstractView.backgroundColor = .yellow
-        // joystickSubstractView.layer.cornerRadius = CGFloat(((substractSize - joystickSize) + substractSize) / 2)
-        scrollView.addSubview(joystickSubstractView)
+        joystickSubstractView.layer.cornerRadius = CGFloat(substractSize / 2)
+        self.view.addSubview(joystickSubstractView)
 
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(dragJoystick))
+        joystickView.isUserInteractionEnabled = true
+        joystickView.addGestureRecognizer(panGesture)
         joystickView.backgroundColor = .red
         joystickView.layer.cornerRadius = CGFloat(joystickSize / 2)
         joystickSubstractView.addSubview(joystickView)
 
-        scrollView.snp.makeConstraints {
+        joystickSubstractView.snp.makeConstraints {
             $0.width.height.equalTo(substractSize)
             $0.centerX.equalToSuperview()
             $0.bottom.equalToSuperview().inset(150)
-        }
-
-        joystickSubstractView.snp.makeConstraints {
-            $0.width.height.equalTo((substractSize - joystickSize) + substractSize)
-            $0.edges.equalToSuperview()
         }
 
         joystickView.snp.makeConstraints {
@@ -55,52 +43,29 @@ class ViewController: UIViewController {
         }
     }
 
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
+    @objc func dragJoystick(_ sender: UIPanGestureRecognizer) {
+        self.view.bringSubviewToFront(joystickView)
+        let translation = sender.translation(in: self.view)
 
-        scrollView.contentOffset = CGPoint(x: (substractSize - joystickSize) / 2, y: (substractSize - joystickSize) / 2)
+        joystickView.center = CGPoint(x: joystickView.center.x + translation.x,
+                                      y: joystickView.center.y + translation.y)
+
+        sender.setTranslation(CGPoint.zero, in: self.view)
     }
 }
 
 extension ViewController: UIScrollViewDelegate {
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        // print("Content offest:      \(scrollView.contentOffset)")
-//        // View center: (207.0, 448.0)
-//        print("Center:    \(self.view.center)")
-//        print("ScrollView center:    \(scrollView.center)")
-//        print("joystickView center:  \(joystickView.convert(joystickView.center, to: self.view))")
-    }
-
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let substractRayon = substractSize / 2
-        let joystickRayon = joystickSize / 2
         let substractCenter = scrollView.center
         let joystickCenter = joystickView.convert(joystickView.center, to: self.view)
         let distanceBetweenCenters = hypot((joystickCenter.x - joystickView.frame.minX) - substractCenter.x,
                                            (joystickCenter.y - joystickView.frame.minY) - substractCenter.y)
 
-        if CGFloat(substractRayon) >= (distanceBetweenCenters + CGFloat(joystickRayon)) {
+        if CGFloat(substractSize / 2) >= (distanceBetweenCenters + CGFloat(joystickSize / 2)) {
             print("Inside")
         } else {
-            let point = CGPoint(x: substractCenter.x + ((joystickCenter.x - joystickView.frame.minX) - substractCenter.x),
-                                y: substractCenter.y + ((joystickCenter.y - joystickView.frame.minY) - substractCenter.y))
-            print("Point:               \(point)")
-            print("ScrollView center:   \(scrollView.center)")
-            print("Offset:              \(scrollView.contentOffset)")
-            print("Test:                \(scrollView.convert(point, to: scrollView))")
-            print()
+            _ = CGPoint(x: substractCenter.x + ((joystickCenter.x - joystickView.frame.minX) - substractCenter.x),
+                        y: substractCenter.y + ((joystickCenter.y - joystickView.frame.minY) - substractCenter.y))
         }
-
-        /*
-        if scrollView.contentOffset.x <= 15 && scrollView.contentOffset.y >= 85 {
-            scrollView.contentOffset = CGPoint(x: 15, y: 85)
-        } else if scrollView.contentOffset.x >= 85 && scrollView.contentOffset.y >= 85 {
-            scrollView.contentOffset = CGPoint(x: 85, y: 85)
-        } else if scrollView.contentOffset.x >= 85 && scrollView.contentOffset.y <= 15 {
-            scrollView.contentOffset = CGPoint(x: 85, y: 15)
-        } else if scrollView.contentOffset.x <= 15 && scrollView.contentOffset.y <= 15 {
-            scrollView.contentOffset = CGPoint(x: 15, y: 15)
-        }
-        */
     }
 }
